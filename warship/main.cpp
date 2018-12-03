6fc23ddb1ca3e54cefd67fb341aa79799cab012d
tsktsktsk64 <teosiaukhian@gmail.com>
2018-12-03 17:41:00 +0800

    Initial Commit

---
+++ warship/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  warship
+//
+//  Created by Nerrons on 3/12/18.
+//  Copyright © 2018 nerrons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
